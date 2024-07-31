b7932df1ed84a8add3d6424ffc2944b9cdd4805e
MaldiniBeh <69940992+MaldiniBeh@users.noreply.github.com>
2024-07-31 02:12:34 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAx-YJcXPt0lLh09Vocf249ujSvO_xqPUY",
-    authDomain: "projetst-romas.firebaseapp.com",
-    projectId: "projetst-romas",
-    storageBucket: "projetst-romas.appspot.com",
-    messagingSenderId: "365627131259",
-    appId: "1:365627131259:web:f207ade0db4962c432e9de",
-    measurementId: "G-3XS6D6JNFK"
+    apiKey: "******",
+    authDomain: "*******",
+    projectId: "******",
+    storageBucket: "********",
+    messagingSenderId: "*******",
+    appId: "******",
+    measurementId: "*******"
   },
 };
